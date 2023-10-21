f704dfba8ee9dc334a3ea8dfd56b3404219e58d7
Mahendra Koirala <60951574+koirala8@users.noreply.github.com>
2023-10-20 19:30:02 -0500

    Using seperate components for clear coding

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import SearchBar from "./Components/SearchBar";
+import SearchResults from "./Components/SearchResults";
 
 function App() {
   const [value, setValue] = useState("");
   const [song, setSong] = useState([]);
   const url = `https://deezerdevs-deezer.p.rapidapi.com/search?q=${value}`;
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'X-RapidAPI-Key': '3b63b8a9a6msh425c9a6cce7556fp1d2331jsnf2540b30c6b2',
-      'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
-    }
+      "X-RapidAPI-Key": "3b63b8a9a6msh425c9a6cce7556fp1d2331jsnf2540b30c6b2",
+      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+    },
   };
 
   function getSuggestions() {
     fetch(url, options)
-      .then(response => response.json())
-      .then(jsonResponse => {
-        const songTitles = jsonResponse.data.map(item => item.title);
+      .then((response) => response.json())
+      .then((jsonResponse) => {
+        const songTitles = jsonResponse.data.map((item) => item.title);
         setSong(songTitles);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Error fetching data:", error);
       });
   }
@@ -29,7 +31,6 @@ function App() {
     e.preventDefault();
     getSuggestions();
   }
-
   function handleChange(e) {
     const newValue = e.target.value;
     setValue(newValue);
@@ -41,20 +42,8 @@ function App() {
 
   return (
     <div>
-      <h1>Jamming</h1>
-      <form onSubmit={handleClick}>
-        <label htmlFor="searchInput">Search</label>
-        <input type="text" id="searchInput" value={value} onChange={handleChange} placeholder='Enter here...' />
-        <button type="submit">Search</button>
-      </form>
-      <h2>Songs</h2>
-      {song && (
-        <ul>
-          {song.map((title, index) => (
-            <li key={index}>{title}</li>
-          ))}
-        </ul>
-      )}
+      <SearchBar handleChange={handleChange} handleClick={handleClick} />
+      <SearchResults song={song} />
     </div>
   );
 }
---
+++ src/Components/SearchResults.js
@@ -0,0 +1,15 @@
+export default function SearchResults(props){
+    
+
+    return (
+        <>
+        <h2>Songs</h2>
+        {props.song && (
+        <ul>
+            {props.song.map((title, index) => (
+            <li key={index}>{title}</li>
+        ))}
+        </ul>
+    )}</>
+    );
+}

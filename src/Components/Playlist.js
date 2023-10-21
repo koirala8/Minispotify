b3bf392e62ac3fd881d8cc94ec961a1a55f3ba29
koirala8 <60951574+koirala8@users.noreply.github.com>
2023-10-21 17:18:20 -0500

    playlist component

--- src/Components/Playlist.js
+++ src/Components/Playlist.js
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
-import React from "react";
 
-export default function Playlist(){
-    return;
-=======
 
 export default function Playlist(props){
 
     return (
         <><h1>Playlist</h1>
-        <p>{`\n${props.playlist}`}</p></>
+        <p>{`${props.playlist}`}</p></>
     );
->>>>>>> 040e800c416088ae5096bb76414c1c37e0c90439
 }
